661420919edb7017d1016eaf85ae844708f4b957
Luc Avronsart <luc.avronsart@prestataires.boulanger.com>
2025-02-06 13:18:31 +0100

    feature(Auth) : design / structure app
    - rework eslint
    - design InputText
    - page auth / login
    - Status
    - Loader
    - firebase errors

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,21 +1,22 @@
-import { Link } from "react-router-dom"
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-import Login from "../../components/Login/Login"
+import Login from '@components/Login/Login'
 
-import "./Auth.scss"
+import './Auth.scss'
 
-const Auth = () => {
+const Auth: React.FC = () => {
   return (
     <section className="auth">
-      <h2>Connexion</h2>
-      <section className="auth__login">
-        <Login />
-      </section>
+      <h1>Connexion</h1>
+      <Login />
       <section className="auth__signup">
-        <p>Pas encore inscrit ? <Link to="/signup">Inscris-toi ici</Link></p>
+        <p>
+          Pas encore inscrit ? <Link to="/signup">Inscris-toi ici</Link>
+        </p>
       </section>
     </section>
   )
 }
 
-export default Auth;
+export default Auth
