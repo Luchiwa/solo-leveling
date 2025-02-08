c45b5c35a2a0566210640c66948b9801a9a7d4be
Luc Avronsart <luc.avronsart@prestataires.boulanger.com>
2025-02-08 20:36:38 +0100

    feature() : realtime level
    - add level function in level utils
    - add error gestion in listening hooks
    - notify on level up
    - get the remaining xp
    - add progress bar for remaining xp
    - update header with slide transition

--- src/utils/levelSystem.ts
+++ src/utils/levelSystem.ts
@@ -1,12 +1,35 @@
+//src/utils/levelSystem.ts
+
 // Fonction pour calculer le niveau en fonction de l'XP totale
 export const calculateLevel = (totalXP: number): number => {
   let level = 0
-  let requiredXP = 0
+  let requiredXP = 1
+  let accumulatedXP = 0
 
-  while (totalXP >= requiredXP) {
+  while (totalXP >= accumulatedXP + requiredXP) {
+    accumulatedXP += requiredXP
     level++
-    requiredXP += level
+    requiredXP = level + 1
   }
 
-  return level - 1 // Retourne le dernier niveau atteint
+  return level
+}
+
+// Fonction pour obtenir l'XP requise pour le prochain niveau
+export const getNextLevelXP = (currentLevel: number): number => {
+  return currentLevel + 1
+}
+
+export const updateXPAndLevel = (currentXP: number, questXP: number) => {
+  const newExperience = currentXP + questXP
+  const newLevel = calculateLevel(newExperience)
+
+  return { newExperience, newLevel }
+}
+
+// Fonction pour calculer l'XP restante avant le prochain niveau
+export const getRemainingXP = (totalXP: number, currentLevel: number): number => {
+  const nextLevelXP = getNextLevelXP(currentLevel) // XP requise pour le prochain niveau
+  const accumulatedXP = (currentLevel * (currentLevel + 1)) / 2 // XP déjà requise pour atteindre le niveau actuel
+  return nextLevelXP - (totalXP - accumulatedXP)
 }
